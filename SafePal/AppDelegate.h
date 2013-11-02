0918ae9eca4d81a24d58816df0abeb99b23edc8e
Laurent Rivard <laurentrivard@gmail.com>
2013-11-02 13:15:14 -0400

    Initial Commit

---
+++ SafePal/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SafePal
+//
+//  Created by Laurent Rivard on 11/2/13.
+//  Copyright (c) 2013 Rivard.Laurent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SafePalTests/SafePalTests.m
@@ -0,0 +1,34 @@
+//
+//  SafePalTests.m
+//  SafePalTests
+//
+//  Created by Laurent Rivard on 11/2/13.
+//  Copyright (c) 2013 Rivard.Laurent. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SafePalTests : XCTestCase
+
+@end
+
+@implementation SafePalTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
